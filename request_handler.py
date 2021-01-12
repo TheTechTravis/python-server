879444bc470a6c72ba29e0ab27523d0f3617ab77
Travis Stevenson <thetechtravis@outlook.com>
2021-01-12 10:35:56 -0600

    Ch. 2 complete

--- request_handler.py
+++ request_handler.py
@@ -1,7 +1,9 @@
 from http.server import BaseHTTPRequestHandler, HTTPServer
-
+from animals import get_all_animals
 
 # Here's a class. It inherits from another class.
+
+
 class HandleRequests(BaseHTTPRequestHandler):
 
     # Here's a class function
@@ -15,8 +17,10 @@ class HandleRequests(BaseHTTPRequestHandler):
     def do_OPTIONS(self):
         self.send_response(200)
         self.send_header('Access-Control-Allow-Origin', '*')
-        self.send_header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE')
-        self.send_header('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept')
+        self.send_header('Access-Control-Allow-Methods',
+                         'GET, POST, PUT, DELETE')
+        self.send_header('Access-Control-Allow-Headers',
+                         'X-Requested-With, Content-Type, Accept')
         self.end_headers()
 
     # Here's a method on the class that overrides the parent's method.
@@ -30,15 +34,11 @@ class HandleRequests(BaseHTTPRequestHandler):
 
         # It's an if..else statement
         if self.path == "/animals":
-            response = [
-                { "id": 1, "name": "Snickers", "species": "Dog" },
-                { "id": 2, "name": "Lenny", "species": "Cat" }
-            ]
-
+            response = get_all_animals()
         else:
             response = []
 
-        # This weird code sends a response back to the client
+    # This weird code sends a response back to the client
         self.wfile.write(f"{response}".encode())
 
     # Here's a method on the class that overrides the parent's method.
@@ -52,9 +52,9 @@ class HandleRequests(BaseHTTPRequestHandler):
         response = f"received post request:<br>{post_body}"
         self.wfile.write(response.encode())
 
-
     # Here's a method on the class that overrides the parent's method.
     # It handles any PUT request.
+
     def do_PUT(self):
         self.do_POST()
 
@@ -65,6 +65,7 @@ def main():
     host = ''
     port = 8088
     HTTPServer((host, port), HandleRequests).serve_forever()
+
 
 if __name__ == "__main__":
     main()
