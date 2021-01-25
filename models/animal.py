55237cd83673b3d94e87e09bed6852b0be9ae1d7
Travis Stevenson <thetechtravis@outlook.com>
2021-01-25 09:18:56 -0600

    Ch. 15, almost finished

--- models/animal.py
+++ models/animal.py
@@ -1,6 +1,6 @@
 class Animal():
 
-    # Class initializer. It has 5 custom parameters, with the
+    # Class initializer. It has 8 custom parameters, with the
     # special `self` parameter that every method on a class
     # needs as the first parameter.
     def __init__(self, id, name, status, breed, customer_id, location_id, location, customer):
